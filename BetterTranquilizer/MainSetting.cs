54a9f4b3f2d6f21973ff9ac6a0327c4119dd90f8
mrzhnex <46461216+mrzhnex@users.noreply.github.com>
2020-07-02 19:43:33 +0300

    Update MainSetting.cs
    Удален комментарий одного дегенерата в основном классе.

--- BetterTranquilizer/MainSetting.cs
+++ BetterTranquilizer/MainSetting.cs
@@ -36,7 +36,6 @@ namespace BetterTranquilizer
             Events.SetClassEvent -= setEvents.OnSetClass;
             Events.WaitingForPlayersEvent -= setEvents.OnWaitingForPlayers;
             Log.Info(getName + " off");
-            //man hello
         }
 
         public override void OnReload() { }
